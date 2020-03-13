fc24c5bc9b04a5367080bbcac7e4838965130a09
akter_rakib <rakibccj@gmail.com>
2020-03-14 00:06:40 +0600

    initiate project

---
+++ src/main/java/com/rakib/coronaappscasestracker/CoronaAppsCasesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rakib.coronaappscasestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaAppsCasesTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaAppsCasesTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rakib/coronaappscasestracker/CoronaAppsCasesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.coronaappscasestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaAppsCasesTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

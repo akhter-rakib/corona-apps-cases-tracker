99251e1840991f33c6ca7c44066da77df8943ee4
akter_rakib <rakibccj@gmail.com>
2020-03-14 01:28:16 +0600

    added Schedule

--- src/main/java/com/rakib/coronaappscasestracker/CoronaAppsCasesTrackerApplication.java
+++ src/main/java/com/rakib/coronaappscasestracker/CoronaAppsCasesTrackerApplication.java
@@ -2,8 +2,10 @@ package com.rakib.coronaappscasestracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronaAppsCasesTrackerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/rakib/coronaappscasestracker/services/CoronaVirusDataService.java
+++ src/main/java/com/rakib/coronaappscasestracker/services/CoronaVirusDataService.java
@@ -2,6 +2,7 @@ package com.rakib.coronaappscasestracker.services;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -18,13 +19,14 @@ public class CoronaVirusDataService {
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL)).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         StringReader reader = new StringReader(response.body());
-        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(reader);
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
         for (CSVRecord record : records) {
             String state = record.get("Province/State");
             System.out.println(state);
